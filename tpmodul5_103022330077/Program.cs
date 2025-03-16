f2237927e34f681c5d767c966f2ec85e76aa4719
gilang <156637129+gilang1223@users.noreply.github.com>
2025-03-16 20:39:14 +0700

    awal

--- tpmodul5_103022330077/Program.cs
+++ tpmodul5_103022330077/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+public class halloGeneric
+{
+    public void SayHello<T>(T name)
+    {
+        Console.WriteLine("Hello, " + name);
+    }
+}
 
+public class Program
+{
+    static void Main(string[] args)
+    {
+        halloGeneric hg = new halloGeneric();
+        hg.SayHello("gilang");
+    }
+}
